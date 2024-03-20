9daeaffcd28547cab392335b7ec835233863d701
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-03-20 19:03:55 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'someone'
+
+function uppercaseCustomerName(){
+
+        customerName = customerName.toUpperCase();
+    }
+
+    function setBestCustomer(){
+        bestCustomer = 'not bob';
+    }
+
+    function overwriteBestCustomer(){
+        bestCustomer = 'maybe bob';
+    }
+
+
+    function changeLeastFavoriteCustomer(){
+        leastFavoriteCustomer = 'someone else';
+    }
